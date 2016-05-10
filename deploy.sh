e53b4b8fdba6df8a79614610ab197b89b2f7235c
Erik Lumpkin <elumpkin@caisgroup.com>
2016-05-10 18:42:07 -0500

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
+echo "committing and pushing changes to git repo"
 git commit -am "auto commit"
 git push
 
+echo "pushing files out to all servers"
 for host in `cat hosts`;
 	do echo deploying to $host;
 	for file in '.bashrc' '.bash_logout' '.bash_profile';
-		do echo scping file $file;
+		do echo scping $file to $host:$file;
+		scp $file $host:$file
 	done;
 done;
